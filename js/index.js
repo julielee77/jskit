b360319f4a20e132c0f5e4e0dc888a22936ca699
Julie <lihaizhu@gstianfu.com>
2016-12-17 01:14:33 +0800

    demo home page

---
+++ js/index.js
@@ -0,0 +1,23 @@
+var vm=new Vue({
+	data:{
+		frameworks:[{
+			name:'vue',
+			github:''
+		},{
+			name:'vue-resource',
+			github:''
+		},{
+			name:'vue-router',
+			github:''
+		},{
+			name:'elmentUI'
+		}]
+
+	},
+	ready:function(){
+
+	},
+	methods:{
+
+	}
+})
